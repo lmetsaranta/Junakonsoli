6b7422a1ca1c86a61c37b1a74653f299bd2ab955
lmetsaranta <lennu_metsaranta@hotmail.com>
2019-06-25 11:07:22 +0300

    Pohja

---
+++ src/main/java/fi/academy/junakonsoli/Main.java
@@ -0,0 +1,13 @@
+package fi.academy.junakonsoli;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
