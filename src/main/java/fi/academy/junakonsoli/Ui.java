e8fd9ef5b18ca19806b0d8f9ff71d4d5d3fbaf2b
Heikki Vihemäki <heikki.vihemaki@gmail.com>
2019-06-27 13:56:34 +0300

    liikkuvien junien tsekkaus

--- src/main/java/fi/academy/junakonsoli/Ui.java
+++ src/main/java/fi/academy/junakonsoli/Ui.java
@@ -24,23 +24,21 @@ public class Ui {
     public void kaynnista() { // heittää mainiin, pitäis varmaan käsitellä siellä?
         System.out.println("Junainfo");
 
-        String komento="";
+        String komento = "";
         do {
             System.out.println(
-                    "\nToiminnot: \n"+
-                    "1: Tarkasta juna-aikataulut kahden aseman välillä \n" +
-                    "2: Hae yksittäisen junan aikataulua numeron perusteella \n" +
-                    "0: Poistu junakonsolista");
+                    "\nToiminnot: \n" +
+                            "1: Tarkasta juna-aikataulut kahden aseman välillä \n" +
+                            "2: Hae yksittäisen junan aikataulua numeron perusteella \n" +
+                            "3: Hae liikkeellä olevia junia \n" +
+                            "0: Poistu junakonsolista");
 
             komento = scanner.nextLine();
             switch (komento) {
                 case "1":
                     System.out.println("Syötä lähtöasema:");
-                    String lahtoAsemanNimi = muokkaa(scanner.nextLine());
-                    System.out.println("Syötä määräasema:");
-                    String maaraAsemanNimi = muokkaa(scanner.nextLine());
-                    lahtoasema=asema.haeAsemanKoodi(lahtoAsemanNimi);
-                    maaraAsema=asema.haeAsemanKoodi(maaraAsemanNimi);
+                    syotaLahtoasema();
+                    syotaMaaraAsema();
                     System.out.println("");
                     tulostaAikataulut(lahtoasema, maaraAsema);
                     System.out.println("");
@@ -60,7 +58,24 @@ public class Ui {
 
                     break;
                 case "3":
-                    //seuraava saapuva juna
+                    System.out.print(" Toiminnot:\n 1: listaa kaikki liikkeellä olevat junat\n 2: hae kahden aseman välissä liikkeellä olevat junat\n");
+                    komento = scanner.nextLine();
+                    if (komento=="1") {
+                        //tähän metodikutsu, joka listaa liikkeellä olevat junat
+                        break;
+                    } if (komento=="2") {
+                        syotaLahtoasema();
+                        syotaMaaraAsema();
+                        // tähän metodikutsu. parametreina oliomuuttujat this.lahtoasema ja this.maaraAsema
+                        break;
+
+
+                }
+
+                    //hae liikkeessä olevia junia
+                    //->> 1: hae kaikki\ liikkeellä olevat junat
+                    //->> 2: hae kahden aseman välillä liikkeessä olevia junia
+                    //      - kysy asemat ja tulosta
                     break;
                 case "4":
                     //asemalta lähteneet
@@ -71,9 +86,27 @@ public class Ui {
         System.out.println("Kiitos ja näkemiin!");
     }
 
+    private void syotaMaaraAsema() {
+        System.out.println("Syötä määräasema:");
+        String maaraAsemanNimi = muokkaa(scanner.nextLine());
+        maaraAsema = asema.haeAsemanKoodi(maaraAsemanNimi);
+        while (maaraAsema == null) {
+            maaraAsemanNimi = muokkaa(scanner.nextLine());
+            maaraAsema = asema.haeAsemanKoodi(maaraAsemanNimi);
+        }
+    }
+
+    private void syotaLahtoasema() {
+        String lahtoAsemanNimi = muokkaa(scanner.nextLine());
+        lahtoasema = asema.haeAsemanKoodi(lahtoAsemanNimi);
+        while (lahtoasema == null) {
+            lahtoAsemanNimi = muokkaa(scanner.nextLine());
+            lahtoasema = asema.haeAsemanKoodi(lahtoAsemanNimi);
+        }
+    }
+
     // Muokataan syötettä -> trimmaus ja jos kaksi osaa niin lisätään se mukaan ja palautetaan.
     public String muokkaa(String syote) {
-
         String[] asemanNimenosat = syote.trim().split(" ");
         String asemanNimi = asemanNimenosat[0].trim().toLowerCase();
         asemanNimi = asemanNimi.substring(0, 1).toUpperCase() + asemanNimi.substring(1);
