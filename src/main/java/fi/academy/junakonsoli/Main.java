c85d8c73e2fdf61eba990b7cbb716c229002312e
kirsi-k <kirsikujala@hotmail.com>
2019-06-27 14:26:42 +0300

    mainissa ascii art

--- src/main/java/fi/academy/junakonsoli/Main.java
+++ src/main/java/fi/academy/junakonsoli/Main.java
@@ -6,6 +6,18 @@ import static fi.academy.junakonsoli.Toiminnallisuus.haeLiikkeessaOlevatJunat;
 public class Main {
 
     public static void main( String[] args ) {
+        String[] ASCIIrivit = new String[7];
+        ASCIIrivit[0]="       _                   _____        __     ";
+        ASCIIrivit[1]="      | |                 |_   _|      / _|     ";
+        ASCIIrivit[2]="      | |_   _ _ __   __ _  | |  _ __ | |_ ___  ";
+        ASCIIrivit[3]="  _   | | | | | '_ \\ / _` | | | | '_ \\|  _/ _ \\ ";
+        ASCIIrivit[4]=" | |__| | |_| | | | | (_| |_| |_| | | | || (_) |";
+        ASCIIrivit[5]="  \\____/ \\__,_|_| |_|\\__,_|_____|_| |_|_| \\___/ ";
+        ASCIIrivit[6]="                                                ";
+
+        for(int i = 0; i < ASCIIrivit.length; i++) {
+            System.out.println(ASCIIrivit[i]);
+        }
         Scanner scanner = new Scanner(System.in);
         Ui ui = new Ui(scanner);
         ui.kaynnista();
