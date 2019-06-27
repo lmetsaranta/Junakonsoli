141b5241453041808e1e58a90f7a72f95003c19d
Heikki Vihemäki <heikki.vihemaki@gmail.com>
2019-06-27 14:20:27 +0300

    case 3 ja 4 pohjia -heikki

--- src/main/java/fi/academy/junakonsoli/Ui.java
+++ src/main/java/fi/academy/junakonsoli/Ui.java
@@ -30,17 +30,15 @@ public class Ui {
                     "\nToiminnot: \n" +
                             "1: Tarkasta juna-aikataulut kahden aseman välillä \n" +
                             "2: Hae yksittäisen junan aikataulua numeron perusteella \n" +
-                            "3: Hae liikkeellä olevia junia \n" +
+                            "3: Hae liikkeellä olevat junat kahden aseman välillä \n" +
+                            "4: Listaa kaikki liikkeellä olevat junat \n" +
                             "0: Poistu junakonsolista");
 
             komento = scanner.nextLine();
             switch (komento) {
                 case "1":
-                    System.out.println("Syötä lähtöasema:");
-                    syotaLahtoasema();
-                    syotaMaaraAsema();
                     System.out.println("");
-                    tulostaAikataulut(lahtoasema, maaraAsema);
+                    tulostaAikataulut(syotaLahtoasema(), syotaMaaraAsema());
                     System.out.println("");
                     break;
 
@@ -58,27 +56,11 @@ public class Ui {
 
                     break;
                 case "3":
-                    System.out.print(" Toiminnot:\n 1: listaa kaikki liikkeellä olevat junat\n 2: hae kahden aseman välissä liikkeellä olevat junat\n");
-                    komento = scanner.nextLine();
-                    if (komento=="1") {
-                        //tähän metodikutsu, joka listaa liikkeellä olevat junat
-                        break;
-                    } if (komento=="2") {
                         syotaLahtoasema();
                         syotaMaaraAsema();
-                        // tähän metodikutsu. parametreina oliomuuttujat this.lahtoasema ja this.maaraAsema
-                        break;
 
-
-                }
-
-                    //hae liikkeessä olevia junia
-                    //->> 1: hae kaikki\ liikkeellä olevat junat
-                    //->> 2: hae kahden aseman välillä liikkeessä olevia junia
-                    //      - kysy asemat ja tulosta
-                    break;
                 case "4":
-                    //asemalta lähteneet
+                    //listaa liikkeellä olevat junat
                     break;
             }
         } while (!komento.equals("0"));
@@ -86,7 +68,7 @@ public class Ui {
         System.out.println("Kiitos ja näkemiin!");
     }
 
-    private void syotaMaaraAsema() {
+    private String syotaMaaraAsema() {
         System.out.println("Syötä määräasema:");
         String maaraAsemanNimi = muokkaa(scanner.nextLine());
         maaraAsema = asema.haeAsemanKoodi(maaraAsemanNimi);
@@ -94,15 +76,18 @@ public class Ui {
             maaraAsemanNimi = muokkaa(scanner.nextLine());
             maaraAsema = asema.haeAsemanKoodi(maaraAsemanNimi);
         }
+        return maaraAsema;
     }
 
-    private void syotaLahtoasema() {
+    private String syotaLahtoasema() {
+        System.out.println("Syötä lähtöasema:");
         String lahtoAsemanNimi = muokkaa(scanner.nextLine());
         lahtoasema = asema.haeAsemanKoodi(lahtoAsemanNimi);
         while (lahtoasema == null) {
             lahtoAsemanNimi = muokkaa(scanner.nextLine());
             lahtoasema = asema.haeAsemanKoodi(lahtoAsemanNimi);
         }
+        return lahtoasema;
     }
 
     // Muokataan syötettä -> trimmaus ja jos kaksi osaa niin lisätään se mukaan ja palautetaan.
